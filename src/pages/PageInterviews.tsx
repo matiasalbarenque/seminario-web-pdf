b76f67d9668300474d17193d0e7083155d0dab1a
Eduardo Guillermo Chobadindegui <edu-choba@hotmail.com>
2024-09-22 21:00:50 -0300

    wip entrevistas

--- src/pages/PageInterviews.tsx
+++ src/pages/PageInterviews.tsx
@@ -12,15 +12,38 @@ export const PageInterviews = () => {
         <Card className="flex-1">
           <CardHeader className="pb-4">
             <h3 className="pl-3 border-l-2 border-primary-300 text-xl font-semibold leading-none tracking-tight">
-              gtrfhsrgtf arthraehh
+              Julieta Zanazzi - Abogada Mg. Derecho de la Ciberseguridad y Entorno Digital
             </h3>
           </CardHeader>
           <CardContent className="flex flex-col gap-2">
             <div className="flex flex-col gap-3">
               <div className="text-md font-open-sans text-gray-500">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptatum commodi veniam, quas magni
-                nemo reprehenderit quasi, facilis nobis omnis at qui saepe minima eos iusto impedit repellat, corporis
-                voluptas.
+                <b>¿Quién sos, que estudiaste?</b>
+
+                <p>“Soy Julieta Zanazzi, soy abogada y soy magister en derecho de la ciberseguridad y entorno digital, especializada en cibercrimen. Actualmente estoy trabajando en la dirección de asistencia judicial en delitos complejos y crímenes organizados en la corte suprema de justicia y, específicamente en el área de las telecomunicaciones, entre ellos, redes y comunicaciones electrónicas en general.”
+                </p>
+                <b>¿Por qué te parece importante que los usuarios le presten atención a los tycs?
+                </b>
+                <p>
+                  “La importancia es que el usuario empiece a entender que hay que proteger nuestros datos y que la info. A este tipo de empresas es plata y se lo estamos dando gratuito, y además que con nuestra info pueden hacer muchísimas cosas, privacidad, suplantación de identidad, cometer algún tipo de delito.”
+                  <br/>"No son nada cómodas para el usuario"
+                  <br/>"Hay que tener lo mínimo para tu comercio y no más"
+                  <br/>"Hay que saber la empresa que está detrás"
+                  <br/>"Me gusta como Europa maneja las cookies"
+                  <br/>"Todo lo que metemos en internet no se borra"
+                  <br/>"Puede tener riesgos en la vida real"
+                </p>
+
+                <b>¿Qué notas en la sociedad con respecto a este problema en los últimos 5-10 años?</b>
+                <p>“La gente empezó a ser más consciente de que hay que proteger nuestra información.”
+                  <br/>“Pero falta un montón todavía igual. Algo es algo.”
+                  <br/>“¿Cuánta gente metió el ojito por 50 lucas?”</p>
+
+                <b>¿Decis que el estado está ausente(World Coin)?</b>
+                <p>“tiene que haber una materia que hable de esto”
+                  “los pibes no tienen conciencia de los riesgos”
+                </p>
+
               </div>
             </div>
           </CardContent>
